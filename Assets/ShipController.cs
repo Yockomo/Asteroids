388385a14ad8518278ecb92d1c56e4de5b41206b
Yockomo <Funohs@yandex.ru>
2022-07-06 18:22:31 +0300

    PlayerMovement

--- Assets/ShipController.cs
+++ Assets/ShipController.cs
@@ -5,18 +5,42 @@ using UnityEngine;
 public class ShipController : MonoBehaviour
 {
     [SerializeField] private float moveSpeed;
+    [SerializeField] private Transform forwardPoint;
 
+    public bool IsMoving { get; set; }
+
+    private InputSystem inputs;
+    private Vector3 direction;
+
+    private void Start()
+    {
+        inputs = GetComponent<InputSystem>();
+    }
 
     private void FixedUpdate()
     {
-        if(Input.GetKeyDown(KeyCode.W))
-            MoveForward();
+        if(inputs.Moving && !IsMoving)
+        {
+            direction = new Vector3(forwardPoint.position.x, forwardPoint.position.y);
+            StartCoroutine(LerpPosition(1/moveSpeed));
+        }
     }
 
-    private void MoveForward()
+    private IEnumerator LerpPosition(float duration)
     {
-        var nextPoint = Vector2.Lerp(transform.position, transform.forward, Time.deltaTime * moveSpeed);
-        Debug.Log(nextPoint);
-        transform.Translate(nextPoint);
+        IsMoving = true;
+        var endPoint = transform.position + (direction - transform.position);
+        var time = 0f;
+        Vector2 startPosition = transform.position;
+
+        while (time < duration)
+        {
+            transform.position = Vector2.Lerp(startPosition, endPoint, time / duration);
+            time += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.position = endPoint;
+        IsMoving = false;
     }
 }
